be4c56fbf37960b8daf139e945085bdde1d01e84
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-08-28 14:56:47 +0530

    Add files via upload

---
+++ Python_Day01 HW01 Square.py
@@ -0,0 +1,23 @@
+# H.W Making of square:
+
+import turtle
+
+p = turtle.Pen()
+p . shape("turtle")
+p . color("red","blue")
+p . begin_fill()
+p . forward(100)
+p . left(90)
+p . forward(100)
+p . left(135)
+p . forward(140)
+p . backward(140)
+p . end_fill()
+p . right(45)
+p . forward(100)
+p . left(90)
+p . forward(100)
+p . left(90)
+
+
+input()
