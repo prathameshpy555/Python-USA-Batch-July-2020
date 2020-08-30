01f9bb5ad06b0db795f93fe49663ceafd97d18a6
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-08-30 14:40:37 +0530

    Add files via upload

---
+++ Python_Day01 HW02 Square.py
@@ -0,0 +1,25 @@
+# Drawing Square using two colors:
+
+import turtle
+m = turtle.Pen()
+m . shape("turtle")
+m . color("red","blue")
+m . begin_fill()
+m . forward(100)
+m . left(90)
+m . forward(100)
+m . left(135)
+m . forward(140)
+m . backward(140)
+m . end_fill()
+m . color("red","green")
+m . begin_fill()
+m . right (45)
+m . forward(100)
+m . left(90)
+m . forward(100)
+m . left(90)
+m . end_fill()
+
+input()
+
