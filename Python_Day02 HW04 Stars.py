1411603b7cc6e05a6d5ea516d4d2db39d49b342b
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-09-02 21:16:38 +0530

    Add files via upload

---
+++ Python_Day02 HW04 Stars.py
@@ -0,0 +1,30 @@
+# Draw a stars:
+
+import turtle
+
+a = turtle.Pen()
+
+a . left(90)
+a . color("white")
+a . begin_fill()
+a . forward(10)
+a . end_fill()
+a . color("black")
+a . width("5")
+a . right(90)
+a.speed(5)
+
+a . left(40)
+a . forward(230)
+a . left(140)
+a . forward(200)
+a . left(140)
+a . forward(230)
+a . left(147)
+a . forward(250)
+a . left(144)
+a . forward(251)
+a . hideturtle()
+
+input("press any key to close...")
+
