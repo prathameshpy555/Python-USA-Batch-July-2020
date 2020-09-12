a93157f6c9263001d96c16fde39b4a66d60e4ab5
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-09-12 16:53:17 +0530

    Calculations using Python.

---
+++ Python_Day04 HW10 Arethmatics.py
@@ -0,0 +1,19 @@
+print(" ")
+print("Input...")
+print(" ")
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = a+b
+d = a-b
+e = a*b
+f =  a/b
+print(" ")
+print("Output...")
+print(" ")
+print("Sum of",a,"and",b,"=",c)
+print("Difference of",a,"and",b,"=",d)
+print("Product of",a,"and",b,"=",e)
+print("Division of",a,"and",b,"=",f)
+
+input()
