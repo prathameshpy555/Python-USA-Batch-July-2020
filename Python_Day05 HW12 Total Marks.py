d79217075c61171bb70c1a5305efe7b1592dfecb
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-09-14 17:34:21 +0530

    Obtaining total marks of a student using Python.

---
+++ Python_Day05 HW12 Total Marks.py
@@ -0,0 +1,13 @@
+print(" ")
+print("Input...")
+print(" ")
+a = int(input("Enter your computer marks : "))
+b = int(input("Enter your Maths marks : "))
+c = int(input("Enter your Science marks : "))
+
+print(" ")
+print("Output...")
+print(" ")
+print("Total marks obtain :",a+b+c)
+
+input()
