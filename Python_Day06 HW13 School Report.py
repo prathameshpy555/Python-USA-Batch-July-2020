7a781f270fd943578ce2fbbccf2a8670f856bbb7
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-09-15 12:30:05 +0530

    Taking out Students school report using Python.

---
+++ Python_Day06 HW13 School Report.py
@@ -0,0 +1,28 @@
+# Wright a program :
+print(" ")
+print("Intput...")
+print(" ")
+a = int(input("Enter your roll number : "))
+b = input("Enter your name : ")
+c = int(input("Enter your English marks : "))
+d = int(input("Enter your Maths marks : "))
+e = int(input("Enter your Science marks : "))
+f = c+d+e
+print(" ")
+print("Output...")
+print(" ")
+print("-"* 80)
+print("\t\t\t\tStudent Report")
+print("-"* 80)
+print("\t. Roll no : ",a )
+print("\t. Name   : ",b )
+print("\t. Eng       : ",c )
+print("\t. Maths  : ",d )
+print("\t. Sci         : ",e )
+              
+print("\t. Total marks : ",f)
+print("\t. AVG : ",f/3)
+print("-"* 80)
+print("-"* 80)
+
+input()
