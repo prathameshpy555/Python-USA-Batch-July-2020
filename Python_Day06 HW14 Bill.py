8cd5c70563fb5e4836f94d2047400c0774d473ab
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-09-16 19:04:13 +0530

    Obtaining Market Bill using Python.

---
+++ Python_Day06 HW14 Bill.py
@@ -0,0 +1,24 @@
+#  Wriite Program to make a  Bill:
+print(" ")
+print("Input...")
+print(" ")
+a = int(input("Enter product number :\t"))
+b = input("Enter product name :\t")
+c = int(input("Enter product price:\t"))
+d = int(input("Enter product quantity :"))
+e = c*d
+
+print(" ")
+print("-"* 80)
+print("\t\t\t\t Bill")
+print("-"* 80)
+print(" ")
+print("\t.Product number:\t",a )
+print("\t.Product name  :\t",b )
+print("\t.Product price :\t",c )
+print("\t.Product quantity :\t",d )
+print("-"* 60)
+print("\tTotal :\t\t\t",e)
+
+
+input()
