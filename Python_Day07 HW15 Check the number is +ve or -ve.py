a02d983714a5273c827df271f4780b148e58a059
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-09-19 18:27:17 +0530

    Checking whether the number is +ve or -ve using Python.

---
+++ Python_Day07 HW15 Check the number is +ve or -ve.py
@@ -0,0 +1,14 @@
+# Check weither the number is positive or negative:
+print("")
+print("Input...")
+print("")
+a = int(input("Input a number :"))
+
+print("")
+print("Output...")
+print("")
+
+if a > 0:
+              print(a,"is a positive number")
+else:
+              print(a,"is a negative number")
