d5be735d2f0c4d66ad896f27441a327502ad48c3
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-10-19 18:00:25 +0530

    Finding greater number using Python.

---
+++ Python_Day07 HW17 Find the greater number.py
@@ -0,0 +1,9 @@
+#Find the greater number :
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+if a >= b :
+              print(a,"is greater than",b)
+else:
+              print(b,"is greater than",a)
