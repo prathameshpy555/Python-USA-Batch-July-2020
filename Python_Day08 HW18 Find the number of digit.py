b692c390688f34a866531d3666617fd7bfe9f84e
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-10-20 21:57:05 +0530

    Finding the number of digit using Python.

---
+++ Python_Day08 HW18 Find the number of digit.py
@@ -0,0 +1,11 @@
+#Find the number of digit :
+
+a = int(input("Enter any number : "))
+
+if a >= 999 and a<=9999 :
+              print(a,"is a 4 digit number.")
+else:
+             print(a,"is not a 4 digit number.")
+
+
+ 
