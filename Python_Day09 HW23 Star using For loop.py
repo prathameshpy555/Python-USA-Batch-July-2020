201be1c52feb736e50d38d6889967c1de2d332d6
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-10-25 21:40:04 +0530

    Drawing star using loop in Python.

---
+++ Python_Day09 HW23 Star using For loop.py
@@ -0,0 +1,22 @@
+
+# Star using For loop:
+
+import turtle
+a=turtle.Pen()
+scr=turtle.Screen()
+scr.setup(600,600)
+a . color("black","brown")
+a . width(5)
+a . begin_fill()
+for i in range (5):
+              a . forward(200)
+              a . right(144)
+a . end_fill()
+a . hideturtle()
+
+input()
+
+
+
+
+
