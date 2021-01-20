e8bb9a65bf61dc6b3baeda208f8816da6396918a
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2021-01-20 15:55:52 +0530

    String based on its lenght

--- Python_Day11 HW29 String based on its lenght.py
+++ Python_Day11 HW29 String based on its lenght.py
@@ -1,1 +1,8 @@
-# Accept a string and display the given string based on it's lenght.
+# WAP accept a string and find length of the string:-
+
+x=input("Enter a string : ") # hari
+cnt=0
+for   i  in  x :
+    cnt=cnt+1
+
+print("Length of ",x,"string is:",cnt)
